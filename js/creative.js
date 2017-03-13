65c3c7b95efb1d18dc86a86c94736e01c1cc318d
mosch <ad.moshchynskyy@gmail.com>
2017-03-13 21:00:55 +0200

    newnewnew

---
+++ js/creative.js
@@ -0,0 +1,28 @@
+var exp = document.getElementById("amount").value;
+var back = document.getElementById("vh_wrapper");
+var rocket = document.getElementById("rocket");
+
+
+function mainClick() {
+
+if (exp == 100) {
+	back.style.cssText="background-position-y:100%;"; 
+	rocket.style.cssText="top:30%;"; 
+} else if (exp == 200) {
+	back.style.cssText="background-position-y:75%;";
+	rocket.style.cssText="top:18%;"; 
+} else if (exp == 300) {
+	back.style.cssText="background-position-y:55%;";
+	rocket.style.cssText="top:6%;"; 
+} else if (exp == 400) {
+	back.style.cssText="background-position-y:30%;";
+	rocket.style.cssText="top:-6%;"; 
+} else {
+	back.style.cssText="background-position-y:0%;";
+	rocket.style.cssText="top:-18%;"; 
+} return;
+
+}
+
+
+// mainClick();
